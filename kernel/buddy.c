65faa25b1a9d0a07095227c314861f71529ec60a
longintlong <747099367@qq.com>
2021-03-21 20:21:21 -0700

    lab3 alloctor

--- kernel/buddy.c
+++ kernel/buddy.c
@@ -49,6 +49,13 @@ void bit_set(char *array, int index) {
   array[index/8] = (b | m);
 }
 
+// Set bit at position index in array to 1
+void new_bit_set(char *array, int index) {
+  char b = array[index/8];
+  char m = (1 << (index % 8));
+  array[index/8] = (b ^ m);
+}
+
 // Clear bit at position index in array
 void bit_clear(char *array, int index) {
   char b = array[index/8];
@@ -139,13 +146,13 @@ bd_malloc(uint64 nbytes)
 
   // Found a block; pop it and potentially split it.
   char *p = lst_pop(&bd_sizes[k].free);
-  bit_set(bd_sizes[k].alloc, blk_index(k, p));
+  new_bit_set(bd_sizes[k].alloc, blk_index(k, p) >> 1);
   for(; k > fk; k--) {
     // split a block at size k and mark one half allocated at size k-1
     // and put the buddy on the free list at size k-1
     char *q = p + BLK_SIZE(k-1);   // p's buddy
     bit_set(bd_sizes[k].split, blk_index(k, p));
-    bit_set(bd_sizes[k-1].alloc, blk_index(k-1, p));
+    new_bit_set(bd_sizes[k-1].alloc, blk_index(k-1, p) >> 1);
     lst_push(&bd_sizes[k-1].free, q);
   }
   release(&lock);
@@ -175,10 +182,16 @@ bd_free(void *p) {
   for (k = size(p); k < MAXSIZE; k++) {
     int bi = blk_index(k, p);
     int buddy = (bi % 2 == 0) ? bi+1 : bi-1;
-    bit_clear(bd_sizes[k].alloc, bi);  // free p at size k
-    if (bit_isset(bd_sizes[k].alloc, buddy)) {  // is buddy allocated?
-      break;   // break out of loop
+
+    new_bit_set(bd_sizes[k].alloc, bi >> 1);
+    if (bit_isset(bd_sizes[k].alloc, buddy >> 1)) {
+      break;
     }
+    
+    //bit_clear(bd_sizes[k].alloc, bi);  // free p at size k
+    // if (bit_isset(bd_sizes[k].alloc, buddy)) {  // is buddy allocated?
+    //   break;   // break out of loop
+    // }
     // budy is free; merge with buddy
     q = addr(k, buddy);
     lst_remove(q);    // remove buddy from free list
@@ -192,6 +205,7 @@ bd_free(void *p) {
   lst_push(&bd_sizes[k].free, p);
   release(&lock);
 }
+
 
 // Compute the first block at size k that doesn't contain p
 int
@@ -229,7 +243,7 @@ bd_mark(void *start, void *stop)
         // if a block is allocated at size k, mark it as split too.
         bit_set(bd_sizes[k].split, bi);
       }
-      bit_set(bd_sizes[k].alloc, bi);
+      new_bit_set(bd_sizes[k].alloc, bi >> 1);
     }
   }
 }
@@ -237,17 +251,27 @@ bd_mark(void *start, void *stop)
 // If a block is marked as allocated and the buddy is free, put the
 // buddy on the free list at size k.
 int
-bd_initfree_pair(int k, int bi) {
+bd_initfree_pair(int k, int bi, void *min_left, void *max_right) {
   int buddy = (bi % 2 == 0) ? bi+1 : bi-1;
   int free = 0;
-  if(bit_isset(bd_sizes[k].alloc, bi) !=  bit_isset(bd_sizes[k].alloc, buddy)) {
+  if(bit_isset(bd_sizes[k].alloc, bi >> 1)){
     // one of the pair is free
     free = BLK_SIZE(k);
-    if(bit_isset(bd_sizes[k].alloc, bi))
-      lst_push(&bd_sizes[k].free, addr(k, buddy));   // put buddy on free list
-    else
-      lst_push(&bd_sizes[k].free, addr(k, bi));      // put bi on free list
+    if(addr(k, buddy) >= min_left && addr(k, buddy) < max_right){
+      lst_push(&bd_sizes[k].free, addr(k, buddy));
+    }
+    else{
+      lst_push(&bd_sizes[k].free, addr(k, bi));
+    }
   }
+  // if(bit_isset(bd_sizes[k].alloc, bi) !=  bit_isset(bd_sizes[k].alloc, buddy)) {
+  //   // one of the pair is free
+  //   free = BLK_SIZE(k);
+  //   if(bit_isset(bd_sizes[k].alloc, bi))
+  //     lst_push(&bd_sizes[k].free, addr(k, buddy));   // put buddy on free list
+  //   else
+  //     lst_push(&bd_sizes[k].free, addr(k, bi));      // put bi on free list
+  // }
   return free;
 }
   
@@ -255,16 +279,16 @@ bd_initfree_pair(int k, int bi) {
 // are only two pairs that may have a buddy that should be on free list:
 // bd_left and bd_right.
 int
-bd_initfree(void *bd_left, void *bd_right) {
+bd_initfree(void *bd_left, void *bd_right, void *min_left, void *max_right) {
   int free = 0;
 
   for (int k = 0; k < MAXSIZE; k++) {   // skip max size
     int left = blk_index_next(k, bd_left);
     int right = blk_index(k, bd_right);
-    free += bd_initfree_pair(k, left);
+    free += bd_initfree_pair(k, left, min_left, max_right);
     if(right <= left)
       continue;
-    free += bd_initfree_pair(k, right);
+    free += bd_initfree_pair(k, right, min_left, max_right);
   }
   return free;
 }
@@ -313,11 +337,13 @@ bd_init(void *base, void *end) {
   bd_sizes = (Sz_info *) p;
   p += sizeof(Sz_info) * nsizes;
   memset(bd_sizes, 0, sizeof(Sz_info) * nsizes);
-
   // initialize free list and allocate the alloc array for each size k
   for (int k = 0; k < nsizes; k++) {
     lst_init(&bd_sizes[k].free);
     sz = sizeof(char)* ROUNDUP(NBLK(k), 8)/8;
+    if(sz >> 1)
+      sz >>= 1;
+
     bd_sizes[k].alloc = p;
     memset(bd_sizes[k].alloc, 0, sz);
     p += sz;
@@ -343,7 +369,7 @@ bd_init(void *base, void *end) {
   void *bd_end = bd_base+BLK_SIZE(MAXSIZE)-unavailable;
   
   // initialize free lists for each size k
-  int free = bd_initfree(p, bd_end);
+  int free = bd_initfree(p, bd_end, p, bd_end);
 
   // check if the amount that is free is what we expect
   if(free != BLK_SIZE(MAXSIZE)-meta-unavailable) {
--- kernel/file.c
+++ kernel/file.c
@@ -16,7 +16,7 @@
 struct devsw devsw[NDEV];
 struct {
   struct spinlock lock;
-  struct file file[NFILE];
+  //struct file file[NFILE];
 } ftable;
 
 void
@@ -25,19 +25,37 @@ fileinit(void)
   initlock(&ftable.lock, "ftable");
 }
 
+//Allocate a file structure.
+// struct file*
+// filealloc(void)
+// {
+//   struct file *f;
+//
+//   acquire(&ftable.lock);
+//   for(f = ftable.file; f < ftable.file + NFILE; f++){
+//     if(f->ref == 0){
+//       f->ref = 1;
+//       release(&ftable.lock);
+//       return f;
+//     }
+//   }
+//   release(&ftable.lock);
+//   return 0;
+// }
+
+
 // Allocate a file structure.
 struct file*
 filealloc(void)
 {
   struct file *f;
-
   acquire(&ftable.lock);
-  for(f = ftable.file; f < ftable.file + NFILE; f++){
-    if(f->ref == 0){
-      f->ref = 1;
-      release(&ftable.lock);
-      return f;
-    }
+  f = (struct file *)bd_malloc(sizeof(struct file));
+  if(f){
+    memset(f, 0, sizeof(struct file));
+    f->ref = 1;
+    release(&ftable.lock);
+    return f;
   }
   release(&ftable.lock);
   return 0;
@@ -80,6 +98,7 @@ fileclose(struct file *f)
     iput(ff.ip);
     end_op(ff.ip->dev);
   }
+  bd_free(f);
 }
 
 // Get metadata about file f.
